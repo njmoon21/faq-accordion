431c6b630079c4e79ce0d76c2bf2f6e6d9dabfc3
Nathan <163361000+njmoon21@users.noreply.github.com>
2025-01-11 19:47:57 -0800

    Added button functionality

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button1 = document.getElementById('faq__button1');
+    const button2 = document.getElementById('faq__button2');
+    const button3 = document.getElementById('faq__button3');
+    const button4 = document.getElementById('faq__button4');
 
+    button1.addEventListener('click', function() {
+        const answer = document.getElementById('answer1');
+            if(answer.hidden){
+                answer.hidden = false;
+            }
+            else{
+                answer.hidden = true;
+            }
+    });
+
+    button2.addEventListener('click', function() {
+        const answer = document.getElementById('answer2');
+            if(answer.hidden){
+                answer.hidden = false;
+            }
+            else{
+                answer.hidden = true;
+            }
+    });
+
+    button3.addEventListener('click', function() {
+        const answer = document.getElementById('answer3');
+            if(answer.hidden){
+                answer.hidden = false;
+            }
+            else{
+                answer.hidden = true;
+            }
+    });
+
+    button4.addEventListener('click', function() {
+        const answer = document.getElementById('answer4');
+            if(answer.hidden){
+                answer.hidden = false;
+            }
+            else{
+                answer.hidden = true;
+            }
+    });
+
+});
